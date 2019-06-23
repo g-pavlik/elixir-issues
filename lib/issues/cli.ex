f229d5455ff9d9b85e5b0a4bf1cab5c81c96e103
Grzegorz Pawlik <grzegorzpawlik@gmail.com>
2019-06-23 11:01:23 +0200

    Sort results

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -24,11 +24,12 @@ defmodule Issues.CLI do
     Issues.GithubIssues.fetch(user, project)
     |> decode_response
     |> convert_to_list_of_maps
+    |> sort_into_ascending_order
   end
 
   def decode_response({:ok, body}), do: body
 
-  def decode_response({}:error, error}) do
+  def decode_response({:error, error}) do
     {:_, message} = List.keyfind(error, "message", 0)
     IO.puts "Error fetching from Github: #{message}"
     Sytsem.halt(2)
@@ -37,6 +38,11 @@ defmodule Issues.CLI do
   def convert_to_list_of_maps(list) do
     list
     |> Enum.map(&Enum.into(&1, Map.new))
+  end
+
+  def sort_into_ascending_order(list_of_issues) do
+    Enum.sort list_of_issues,
+              fn i1, i2 -> i1["created_at"] <= i2["created_at"] end
   end
 
   @doc """
